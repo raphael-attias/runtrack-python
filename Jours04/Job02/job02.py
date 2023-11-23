c1db7dcf60febb8269065bc3ea23222700c660f2
raphael.attias <raphael.attias@laplateforme.io>
2023-11-23 10:31:14 +0100

    maj job05

---
+++ Jours04/Job02/job02.py
@@ -0,0 +1,6 @@
+def list():
+    fruits = ['pomme', 'cerise', 'orange']
+    return fruits
+
+fruits_list = list()
+print(fruits_list[1])
---
+++ Jours04/Job05/job05.py
@@ -0,0 +1,10 @@
+def list():
+    nbr = [1, 2, 3, 4, 5]
+    print(nbr_list)
+    print(nbr_list[1])
+    nbr[3] = nbr[2] + nbr[4]
+    print(nbr_list)
+    print(nbr_list[4])
+
+nbr_list = list()
+
