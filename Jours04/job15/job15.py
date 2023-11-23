39df9c8b4fb37a9dbbb2fff226e40b66e1ace447
raphael.attias <raphael.attias@laplateforme.io>
2023-11-23 17:29:44 +0100

    up job15

--- Jours04/job15/job15.py
+++ Jours04/job15/job15.py
@@ -1,14 +1,20 @@
 def tri_bulles(arr):
-    n = len(arr)
+    n = 0
+    for _ in arr:
+        n += 1
+
     for i in range(n):
         for j in range(0, n - i - 1):
             if arr[j] > arr[j + 1]:
-
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
 
 def arrondir_et_trier_liste(liste):
-    for i in range(len(liste)):
+    n = 0
+    for _ in liste:
+        n += 1
+
+    for i in range(n):
         entier = int(liste[i])
         decimale = liste[i] - entier
         if decimale >= 0.5:
@@ -16,8 +22,8 @@ def arrondir_et_trier_liste(liste):
         else:
             liste[i] = entier
     tri_bulles(liste)
-    
-    
+
+
 ma_liste = [22.4, 4.0, 16.22, 9.10, 11.00, 12.22, 14.20, 5.20, 17.50]
 arrondir_et_trier_liste(ma_liste)
 print("Liste arrondie et triée :", ma_liste)
